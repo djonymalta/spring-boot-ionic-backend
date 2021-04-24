8095891449423d77c2d33e7289b99031075dc861
djonymalta <djonymalta@gmail.com>
2021-04-24 11:00:41 -0300

    Ajustando e testando a deleção de cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -1,11 +1,19 @@
 package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.Email;
 
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="E-mail inválido")
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
