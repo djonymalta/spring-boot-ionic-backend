b41fdf03ffd1c5f28093f33e26f143b612002dcb
unknown <djonymalta>
2021-04-30 15:05:36 -0300

    subtotal e total do Pedido

--- src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
@@ -1,5 +1,12 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.List;
+
+import com.nelioalves.cursomc.domain.Produto;
+import com.nelioalves.cursomc.dto.ProdutoDTO;
+import com.nelioalves.cursomc.resources.utils.URL;
+import com.nelioalves.cursomc.services.ProdutoService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-import com.nelioalves.cursomc.domain.Categoria;
-import com.nelioalves.cursomc.domain.Produto;
-import com.nelioalves.cursomc.dto.CategoriaDTO;
-import com.nelioalves.cursomc.dto.ProdutoDTO;
-import com.nelioalves.cursomc.resources.utils.URL;
-import com.nelioalves.cursomc.services.ProdutoService;
 
 @RestController
 @RequestMapping(value = "/produtos")
